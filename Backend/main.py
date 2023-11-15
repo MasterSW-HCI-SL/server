cc84a8b7444ea9446f1872d695ee88391861a2d1
Jonas Svare <jsvare19@student.aau.dk>
2023-11-15 13:27:02 +0100

    Small changes

--- Backend/main.py
+++ Backend/main.py
@@ -10,17 +10,17 @@ app = FastAPI(root_path="/")
 app.mount("/", StaticFiles(directory = "../../Web/dist", html = True), name = "frontend")
 
 # Serving the ML model
-# TODO: Serve 2 files. One .json, and one .bin (.tflite?)
+# TODO: Make sure directory path is correct after updating model
 app.mount("/model", StaticFiles(directory="../../hand-gesture-recognition-trainer/model/keypoint_classifier"), name = "model")
 
 # Serving the FSA
 #TODO: Insert correct directory path
-app.mount("/FSA", StaticFiles(directory = ""), name = "FSA")
+#app.mount("/FSA", StaticFiles(directory = ""), name = "FSA")
 
 
 @app.exception_handler(404)
 def exception_404(input1, input2):
     return RedirectResponse('/')
 
-
-uvicorn.run(app)
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
